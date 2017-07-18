8bb9a55d3cb359caa09c45a032d8b3fbf24450ce
Felipe B. Almeida <felipe.almeida@gmail.com>
2017-07-18 18:31:52 -0300

    including CDS samples

---
+++ src/CDS/CDS.cs
@@ -0,0 +1,93 @@
+﻿/*
+ Copyright (C) 2008 Jose Aparicio
+ Copyright (C) 2014 Felipe Almeida(felipe.almeida@gmail.com)
+ Copyright (C) 2008, 2009 , 2010 Andrea Maggiulli (a.maggiulli@gmail.com)  
+ * 
+ This file is part of QLNet Project http://qlnet.sourceforge.net/
+
+ QLNet is free software: you can redistribute it and/or modify it
+ under the terms of the QLNet license.  You should have received a
+ copy of the license along with this program; if not, license is  
+ available online at <http://qlnet.sourceforge.net/License.html>.
+  
+ QLNet is a based on QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+ The QuantLib license is available online at http://quantlib.org/license.shtml.
+ 
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QLNet;
+using QLNet.Termstructures.Credit;
+
+namespace CDS
+{
+   class CDS
+   {
+      static void Main(string[] args)
+      {
+
+         Calendar calendar = new TARGET();
+         Date todaysDate = new Date(15, Month.May, 2007);
+         // must be a business day
+         todaysDate = calendar.adjust(todaysDate);
+
+
+         // nothing to do with Date::todaysDate
+         Settings.setEvaluationDate(todaysDate);
+
+         // dummy curve
+         double flatQuote = 0.01;
+         Quote flatQuoteRate = new SimpleQuote(flatQuote);
+         FlatForward flatForward = new FlatForward(todaysDate, flatQuoteRate, new Actual365Fixed());
+         Handle<YieldTermStructure> tsCurve = new Handle<YieldTermStructure>(flatForward);
+
+         /*
+          In Lehmans Brothers "guide to exotic credit derivatives"
+          p. 32 there's a simple case, zero flat curve with a flat CDS
+          curve with constant market spreads of 150 bp and RR = 50%
+          corresponds to a flat 3% hazard rate. The implied 1-year
+          survival probability is 97.04% and the 2-years is 94.18%
+         */
+         double recovery_rate = 0.5;
+         double[] quoted_spreads = { 0.0150, 0.0150, 0.0150, 0.0150 };
+
+         List<Period> tenors = new List<Period>();
+         tenors.Add(new Period(3, TimeUnit.Months));
+         tenors.Add(new Period(6, TimeUnit.Months));
+         tenors.Add(new Period(1, TimeUnit.Years));
+         tenors.Add(new Period(2, TimeUnit.Years));
+
+         List<Date> maturities = new List<Date>();
+         for (int i = 0; i < tenors.Count; i++)
+         {
+            maturities.Add(calendar.adjust(todaysDate + tenors[i], BusinessDayConvention.Following));
+         }
+
+         List<RelativeDateDefaultProbabilityHelper> instruments = new List<RelativeDateDefaultProbabilityHelper>();
+
+         for (int i = 0; i < maturities.Count; i++)
+         {
+            instruments.Add(new SpreadCdsHelper(new Handle<Quote>(new SimpleQuote(quoted_spreads[i])),
+               tenors[i],
+               0,
+               calendar,
+               Frequency.Quarterly,
+               BusinessDayConvention.Following,
+               DateGeneration.Rule.TwentiethIMM,
+               new Actual365Fixed(),
+               recovery_rate,
+               tsCurve));
+
+         }
+
+         PiecewiseYieldCurve<Haza
+      }
+   }
+}
---
+++ src/QLNet/Termstructures/Credit/CdsHelper.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QLNet.DateGeneration;
+
+namespace QLNet.Termstructures.Credit
+{
+
+   public class DefaultProbabilityHelper : BootstrapHelper<DefaultProbabilityTermStructure>
+   {
+      public DefaultProbabilityHelper() : base() { } // required for generics
+      public DefaultProbabilityHelper(Handle<Quote> quote) : base(quote) { }
+      public DefaultProbabilityHelper(double quote) : base(quote) { }
+
+   }
+
+   public class RelativeDateDefaultProbabilityHelper : RelativeDateBootstrapHelper<DefaultProbabilityTermStructure>
+   {
+      public RelativeDateDefaultProbabilityHelper() : base() { } // required for generics
+      public RelativeDateDefaultProbabilityHelper(Handle<Quote> quote) : base(quote) { }
+      public RelativeDateDefaultProbabilityHelper(double quote) : base(quote) { }
+   }
+
+   public class CdsHelper : RelativeDateDefaultProbabilityHelper
+   {
+
+      protected Period tenor_;
+      protected int settlementDays_;
+      protected Calendar calendar_;
+      protected Frequency frequency_;
+      protected BusinessDayConvention paymentConvention_;
+      protected Rule rule_;
+      protected DayCounter dayCounter_;
+      protected double recoveryRate_;
+      protected Handle<YieldTermStructure> discountCurve_;
+      protected bool settlesAccrual_;
+      protected bool paysAtDefaultTime_;
+
+      protected Schedule schedule_;
+      protected CreditDefaultSwap swap_;
+      protected RelinkableHandle<DefaultProbabilityTermStructure> probability_;
+      //! protection effective date.
+      protected Date protectionStart_;
+
+      public CdsHelper(Handle<Quote> quote,
+                   Period tenor,
+                   int settlementDays,
+                   Calendar calendar,
+                   Frequency frequency,
+                   BusinessDayConvention paymentConvention,
+                   Rule rule,
+                   DayCounter dayCounter,
+                   double recoveryRate,
+                   Handle<YieldTermStructure> discountCurve,
+                   bool settlesAccrual = true,
+                   bool paysAtDefaultTime = true) : base(quote)
+      {
+
+         tenor_ = tenor;
+         settlementDays_ = settlementDays;
+         calendar_ = calendar;
+         frequency_ = frequency;
+         paymentConvention_ = paymentConvention;
+         rule_ = rule;
+         dayCounter_ = dayCounter;
+         recoveryRate_ = recoveryRate;
+         discountCurve_ = discountCurve;
+         settlesAccrual_ = settlesAccrual;
+         paysAtDefaultTime_ = paysAtDefaultTime;
+
+         initializeDates();
+         discountCurve_.registerWith(update);
+      }
+
+      public CdsHelper(double quote,
+                  Period tenor,
+                  int settlementDays,
+                  Calendar calendar,
+                  Frequency frequency,
+                  BusinessDayConvention paymentConvention,
+                  Rule rule,
+                  DayCounter dayCounter,
+                  double recoveryRate,
+                  Handle<YieldTermStructure> discountCurve,
+                  bool settlesAccrual = true,
+                  bool paysAtDefaultTime = true) : base(quote)
+      {
+         tenor_ = tenor;
+         settlementDays_ = settlementDays;
+         calendar_ = calendar;
+         frequency_ = frequency;
+         paymentConvention_ = paymentConvention;
+         rule_ = rule;
+         dayCounter_ = dayCounter;
+         recoveryRate_ = recoveryRate;
+         discountCurve_ = discountCurve;
+         settlesAccrual_ = settlesAccrual;
+         paysAtDefaultTime_ = paysAtDefaultTime;
+
+         initializeDates();
+         discountCurve_.registerWith(update);
+      }
+
+      public override void setTermStructure(DefaultProbabilityTermStructure ts)
+      {
+         base.setTermStructure(ts);
+         probability_ = new RelinkableHandle<DefaultProbabilityTermStructure>();
+         probability_.linkTo(ts);
+         resetEngine();
+      }
+
+      public override void update()
+      {
+         base.update();
+         resetEngine();
+      }
+      protected override void initializeDates()
+      {
+         protectionStart_ = evaluationDate_ + settlementDays_;
+         Date startDate = calendar_.adjust(protectionStart_, paymentConvention_);
+         Date endDate = evaluationDate_ + tenor_;
+
+         schedule_ = new MakeSchedule().from(startDate)
+                          .to(endDate)
+                          .withFrequency(frequency_)
+                          .withCalendar(calendar_)
+                          .withConvention(paymentConvention_)
+                          .withTerminationDateConvention(BusinessDayConvention.Unadjusted)
+                          .withRule(rule_).value();
+
+         earliestDate_ = schedule_.dates().FirstOrDefault();
+         latestDate_ = calendar_.adjust(schedule_.dates().LastOrDefault(), paymentConvention_);
+      }
+      protected virtual void resetEngine() { }
+   }
+
+   //! Spread-quoted CDS hazard rate bootstrap helper.
+   public class SpreadCdsHelper : CdsHelper
+   {
+      public SpreadCdsHelper(Handle<Quote> runningSpread,
+                        Period tenor,
+                        int settlementDays,
+                        Calendar calendar,
+                        Frequency frequency,
+                        BusinessDayConvention paymentConvention,
+                        Rule rule,
+                        DayCounter dayCounter,
+                        double recoveryRate,
+                        Handle<YieldTermStructure> discountCurve,
+                        bool settlesAccrual = true,
+                        bool paysAtDefaultTime = true) : base(runningSpread, tenor, settlementDays, calendar, frequency, paymentConvention, rule, dayCounter, recoveryRate, discountCurve, settlesAccrual, paysAtDefaultTime)
+      {
+      }
+
+      public SpreadCdsHelper(double runningSpread,
+                        Period tenor,
+                        int settlementDays,
+                        Calendar calendar,
+                        Frequency frequency,
+                        BusinessDayConvention paymentConvention,
+                        Rule rule,
+                        DayCounter dayCounter,
+                        double recoveryRate,
+                        Handle<YieldTermStructure> discountCurve,
+                        bool settlesAccrual = true,
+                        bool paysAtDefaultTime = true) : base(runningSpread, tenor, settlementDays, calendar, frequency, paymentConvention, rule, dayCounter, recoveryRate, discountCurve, settlesAccrual, paysAtDefaultTime)
+      {
+      }
+
+      public override double impliedQuote()
+      {
+         swap_.recalculate();
+         return swap_.fairSpread();
+      }
+
+      protected override void resetEngine()
+      {
+         swap_ = new CreditDefaultSwap(Protection.Side.Buyer, 100.0, 0.01,
+                                         schedule_, paymentConvention_,
+                                         dayCounter_, settlesAccrual_,
+                                         paysAtDefaultTime_,
+                                         protectionStart_);
+
+         swap_.setPricingEngine(new MidPointCdsEngine(probability_,
+                                                      recoveryRate_,
+                                                      discountCurve_));
+      }
+   }
+
+   //! Upfront-quoted CDS hazard rate bootstrap helper.
+   public class UpfrontCdsHelper : CdsHelper
+   {
+      private int upfrontSettlementDays_;
+      Date upfrontDate_;
+      double runningSpread_;
+
+      /*! \note the upfront must be quoted in fractional units. */
+      UpfrontCdsHelper(Handle<Quote> upfront,
+                         double runningSpread,
+                         Period tenor,
+                         int settlementDays,
+                         Calendar calendar,
+                         Frequency frequency,
+                         BusinessDayConvention paymentConvention,
+                         Rule rule,
+                         DayCounter dayCounter,
+                         double recoveryRate,
+                         Handle<YieldTermStructure> discountCurve,
+                         int upfrontSettlementDays = 0,
+                         bool settlesAccrual = true,
+                         bool paysAtDefaultTime = true) : base(upfront, tenor, settlementDays, calendar, frequency, paymentConvention, rule, dayCounter, recoveryRate, discountCurve, settlesAccrual, paysAtDefaultTime)
+      {
+         upfrontSettlementDays_ = upfrontSettlementDays;
+         runningSpread_ = runningSpread;
+
+         initializeDates();
+      }
+
+      /*! \note the upfront must be quoted in fractional units. */
+      UpfrontCdsHelper(double upfront,
+                       double runningSpread,
+                          Period tenor,
+                         int settlementDays,
+                         Calendar calendar,
+                         Frequency frequency,
+                         BusinessDayConvention paymentConvention,
+                         Rule rule,
+                         DayCounter dayCounter,
+                         double recoveryRate,
+                         Handle<YieldTermStructure> discountCurve,
+                         int upfrontSettlementDays = 0,
+                         bool settlesAccrual = true,
+                         bool paysAtDefaultTime = true) : base(runningSpread, tenor, settlementDays, calendar, frequency, paymentConvention, rule, dayCounter, recoveryRate, discountCurve, settlesAccrual, paysAtDefaultTime)
+      {
+         upfrontSettlementDays_ = upfrontSettlementDays;
+         runningSpread_ = runningSpread;
+
+         initializeDates();
+      }
+
+      public override double impliedQuote()
+      {
+         // Testing forward performance option greeks
+         SavedSettings backup = new SavedSettings();
+
+         Settings.includeTodaysCashFlows = true;
+         swap_.recalculate();
+         return swap_.fairUpfront();
+      }
+
+      protected override void initializeDates()
+      {
+         base.initializeDates();
+         upfrontDate_ = calendar_.advance(evaluationDate_,
+                                         upfrontSettlementDays_, TimeUnit.Days,
+                                         paymentConvention_);
+      }
+      protected override void resetEngine()
+      {
+         swap_ = new CreditDefaultSwap(Protection.Side.Buyer, 100.0,
+                                               0.01, runningSpread_,
+                                               schedule_, paymentConvention_,
+                                               dayCounter_,
+                                               settlesAccrual_,
+                                               paysAtDefaultTime_,
+                                               protectionStart_,
+                                               upfrontDate_);
+
+         swap_.setPricingEngine(new MidPointCdsEngine(probability_,
+                                                             recoveryRate_,
+                                                             discountCurve_,
+                                                             true));
+      }
+   }
+
+}
